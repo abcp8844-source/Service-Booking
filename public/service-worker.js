e08678d3f8ffdc23d1e4337957758d89961de711
abcp8844-source <abcp8844@gmail.com>
2026-06-26 22:33:39 +0700

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,29 @@
-const CACHE_NAME = 'booking-hub-v1';
+const CACHE_NAME = 'booking-service-v1';
 const URLS_TO_CACHE = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/manifest.json',
+  '/favicon.jpg'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
