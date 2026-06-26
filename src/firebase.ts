579f7d47e7899046051de9729b214013c50ff965
abcp8844-source <abcp8844@gmail.com>
2026-06-26 19:31:02 +0700

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY?.trim(),
-  // نیچے والی لائن کو براہِ راست ایسے لکھیں، بغیر import.meta.env کے
-  authDomain: "service-booking-42387.firebaseapp.com", 
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN?.trim(),
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID?.trim(),
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET?.trim(),
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID?.trim(),
   appId: import.meta.env.VITE_FIREBASE_APP_ID?.trim(),
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID?.trim()
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
